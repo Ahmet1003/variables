d75401c61c8b19ec9f19debf127a0e2b7f303f46
Ahmet1003 <34312116+Ahmet1003@users.noreply.github.com>
2024-10-25 22:36:53 +0300

    Add files via upload

---
+++ variables/src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int age = 30;
+        age = 35; // We can change the value like this.
+        // If we want to initialize multiple variables at the same time, we should initialize or declare them on separate lines. As following;
+        int temperature = 50;
+        System.out.println(age);
+
+        // Also we can copy a variable's value to another variable. As following;
+        int myAge = 28;
+        int herAge = myAge;
+        System.out.println(myAge);
+
+        // When we give a name to a variable, we have to use camelCasing. So we should start with a capital letter on each except the first word.
+    }
+}
